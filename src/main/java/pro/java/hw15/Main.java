caa43ec93dbbde52577bfc667ab4d6479529361c
Kajihan <burov.arthur@gmail.com>
2023-10-03 07:47:52 +0300

    task 15 completed

---
+++ src/main/java/pro/java/hw15/Main.java
@@ -0,0 +1,9 @@
+package pro.java.hw15;
+
+public class Main {
+
+    public static void main(String[] args) {
+        ValueCalculator calculator = new ValueCalculator(5000000);
+        calculator.fillAndSplitArray();
+    }
+}
