eb524e7f365bb403c1055c077f476953e81b6260
Kajihan <burov.arthur@gmail.com>
2023-12-01 15:25:01 +0200

    ComponentScan annotation was deleted

--- src/main/java/pro/java/newsAPI/src/main/java/api/news/newsapi/config/Config.java
+++ src/main/java/pro/java/newsAPI/src/main/java/api/news/newsapi/config/Config.java
@@ -1,11 +1,9 @@
 package api.news.newsapi.config;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan(basePackages = "api.news.newsapi")
 public class Config {
 
     @Value("${api.news.key}")
